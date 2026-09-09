229e005fcaf1b9b3bd29796400d975f8d27ad51b
Satvik Roy <roysatvik2507@gmail.com>
2026-09-09 22:59:23 +0530

    Add: set-zeroes-in-matrix - submission-0

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool firstColZero = false;
+        
+        for (int r = 0; r < m; r++) {
+            if (matrix[r][0] == 0) {
+                firstColZero = true;
+            }
+            for (int c = 1; c < n; c++) {
+                if (matrix[r][c] == 0) {
+                    matrix[0][c] = 0;
+                    matrix[r][0] = 0;
+                }
+            }
+        }
+        
+        for (int r = 1; r < m; r++) {
+            for (int c = 1; c < n; c++) {
+                if (matrix[0][c] == 0 || matrix[r][0] == 0) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+        
+        if (matrix[0][0] == 0) {
+            for (int c = 0; c < n; c++) {
+                matrix[0][c] = 0;
+            }
+        }
+        
+        if (firstColZero) {
+            for (int r = 0; r < m; r++) {
+                matrix[r][0] = 0;
+            }
+        }
+    }
+};
