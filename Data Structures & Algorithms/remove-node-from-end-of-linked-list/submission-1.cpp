dbe915f2dffa19268624ca4546132c9530e0c146
Satvik Roy <roysatvik2507@gmail.com>
2026-09-14 00:09:29 +0530

    Update: remove-node-from-end-of-linked-list - submission-1

--- Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-1.cpp
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-1.cpp
@@ -11,40 +11,27 @@
 
 class Solution {
 public:
-    void reorderList(ListNode* head) {
-        if (!head || !head->next) return;
-
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        while (fast && fast->next) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-
-        ListNode* secondHalf = slow->next;
-        slow->next = nullptr;
-
-        ListNode* prev = nullptr;
-        ListNode* curr = secondHalf;
-        while (curr) {
-            ListNode* nextTemp = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nextTemp;
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode dummy(0);
+        dummy.next = head;
+        
+        ListNode* left = &dummy;
+        ListNode* right = &dummy;
+        
+        for (int i = 0; i < n; i++) {
+            right = right->next;
         }
         
-        ListNode* firstHalf = head;
-        secondHalf = prev;
-
-        while (secondHalf) {
-            ListNode* temp1 = firstHalf->next;
-            ListNode* temp2 = secondHalf->next;
-            
-            firstHalf->next = secondHalf;
-            secondHalf->next = temp1;
-            
-            firstHalf = temp1;
-            secondHalf = temp2;
+        while (right->next != nullptr) {
+            left = left->next;
+            right = right->next;
         }
+        
+        ListNode* toDelete = left->next;
+        left->next = left->next->next;
+        
+        delete toDelete;
+        
+        return dummy.next;
     }
 };
