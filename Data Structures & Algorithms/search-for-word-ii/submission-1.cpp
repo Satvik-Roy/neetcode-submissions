0a8b591bbd6d2c06b5af16018f67da6be7636257
Satvik Roy <roysatvik2507@gmail.com>
2026-09-16 22:11:31 +0530

    Update: search-for-word-ii - submission-1

--- Data Structures & Algorithms/search-for-word-ii/submission-1.cpp
+++ Data Structures & Algorithms/search-for-word-ii/submission-1.cpp
@@ -1,63 +1,61 @@
-class TrieNode {
-public:
-    TrieNode* children[26];
-    bool isWord;
-    TrieNode() {
-        isWord = false;
-        for (int i = 0; i < 26; i++) children[i] = nullptr;
-    }
-};
-
-class Solution {
-    int m, n;
-    int dirs[4][2] = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-    vector<string> res;
-
-    void dfs(vector<vector<char>>& board, int r, int c, TrieNode* node, string word) {
-        if (r < 0 || r >= m || c < 0 || c >= n) return;
-        
-        char ch = board[r][c];
-        
-        if (ch == '#' || node->children[ch - 'a'] == nullptr) return;
-
-        node = node->children[ch - 'a'];
-        word += ch;
-
-        if (node->isWord) {
-            res.push_back(word);
-            node->isWord = false;
+class WordDictionary {
+private:
+    class TrieNode {
+    public:
+        TrieNode* children[26];
+        bool isWord;
+        TrieNode() {
+            isWord = false;
+            for (int i = 0; i < 26; i++) children[i] = nullptr;
         }
-
-        board[r][c] = '#';
-
-        for (int i = 0; i < 4; i++) {
-            dfs(board, r + dirs[i][0], c + dirs[i][1], node, word);
+    };
+    
+    TrieNode* root;
+    
+    bool dfs(int index, TrieNode* node, string& word) {
+        if (index == word.length()) {
+            return node->isWord;
         }
-
-        board[r][c] = ch;
-    }
-
-public:
-    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        m = board.size();
-        n = board[0].size();
         
-        TrieNode* root = new TrieNode();
-        for (string w : words) {
-            TrieNode* curr = root;
-            for (char c : w) {
-                if (!curr->children[c - 'a']) curr->children[c - 'a'] = new TrieNode();
-                curr = curr->children[c - 'a'];
+        char c = word[index];
+        
+        if (c == '.') {
+            for (int i = 0; i < 26; i++) {
+                if (node->children[i] != nullptr) {
+                    if (dfs(index + 1, node->children[i], word)) {
+                        return true;
+                    }
+                }
             }
-            curr->isWord = true;
-        }
-
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                dfs(board, r, c, root, "");
+            return false;
+        } 
+        else {
+            int idx = c - 'a';
+            if (node->children[idx] == nullptr) {
+                return false;
             }
+            return dfs(index + 1, node->children[idx], word);
         }
+    }
 
-        return res;
+public:
+    WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    void addWord(string word) {
+        TrieNode* curr = root;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (!curr->children[idx]) {
+                curr->children[idx] = new TrieNode();
+            }
+            curr = curr->children[idx];
+        }
+        curr->isWord = true;
+    }
+    
+    bool search(string word) {
+        return dfs(0, root, word);
     }
 };
